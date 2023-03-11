84b8dc10994404b8d2066c15b697620f0ab1fadd
bsung20 <127573617+bsung20@users.noreply.github.com>
2023-03-11 09:18:55 -0500

    create array of rows

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ async fn main(){
     call_api().await;
 }
 
+struct Row{
+    LZ_HOUSTON: f32, 
+    LZ_NORTH: f32, 
+    LZ_SOUTH: f32, 
+    LZ_WEST: f32
+}
+
 async fn call_api() -> Result<(),reqwest::Error>{
     let url = "https://www.ercot.com/content/cdr/html/20230308_dam_spp.html";
 
@@ -18,11 +25,16 @@ async fn call_api() -> Result<(),reqwest::Error>{
 
     let document = Html::parse_document(&res);
 
-    let selector = Selector::parse("td").unwrap();
+    let selector = Selector::parse("tr").unwrap();
 
     for element in document.select(&selector){
-        let m = element.text().last();
-        println!("{:?}", m);
+        let row = element.text().collect::<Vec<_>>();
+
+        let filtered_row = row.iter()
+            .filter(|&&i| i != "\n\t\t" && i != "\n\t")
+            .collect::<Vec<_>>();
+
+        println!("{:?}", filtered_row);
     }
 
     // print!("{:?}", res);
