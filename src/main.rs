c3ae7a713f237371c17bc35c4e2890c4308d2e23
bsung20 <127573617+bsung20@users.noreply.github.com>
2023-03-11 10:53:07 -0500

    create local date url

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use scraper::{Html,Selector};
 use std::error::Error;
 use csv::Writer;
+use chrono::prelude::*;
 
 #[derive(serde::Serialize)]
 struct Prices{
@@ -14,12 +15,30 @@ struct Prices{
 #[tokio::main]
 async fn main(){
 
-    let response = call_api().await;
+    let url = create_local_date_url();
+    print!("{}", url);
+
+    let response = call_api(url).await;
     match response {
         Ok(res) => write_csv(&res.to_owned(), "prices.csv".to_string()),
         Err(e) => {Ok(print!("Could not reach api: {}", e))}
     };
 
+}
+
+fn create_local_date_url() -> String {
+    let local_date_time = Local::now();
+
+    let month = local_date_time.month().to_string();
+    let day = local_date_time.day().to_string();
+    let year = local_date_time.year().to_string();
+
+    let front_url = "https://www.ercot.com/content/cdr/html/";
+    let back_url = "_dam_spp.html";
+
+    let full_url = front_url.to_owned() + &year + &month + &day + back_url;
+
+    full_url
 }
 
 fn write_csv(response: &str, path: String) -> Result<(), Box<dyn Error>> {
@@ -31,6 +50,7 @@ fn write_csv(response: &str, path: String) -> Result<(), Box<dyn Error>> {
 
 
     for element in document.select(&selector){
+
         let row = element.text().collect::<Vec<_>>();
 
         let filtered_row = row.iter()
@@ -38,25 +58,22 @@ fn write_csv(response: &str, path: String) -> Result<(), Box<dyn Error>> {
             .collect::<Vec<_>>();
 
         if filtered_row[0] != &"Oper Day" {
-
             wtr.serialize(Prices{
                 lz_houston : filtered_row[11].parse::<f32>().unwrap(),
                 lz_north : filtered_row[13].parse::<f32>().unwrap(),
                 lz_south : filtered_row[15].parse::<f32>().unwrap(),
                 lz_west : filtered_row[16].parse::<f32>().unwrap()
             })?;
-            // println!("{:?}", prices.lz_houston);
         }
     }
 
     wtr.flush()?;
+
     Ok(())
 }
 
 
-async fn call_api() -> Result<String, reqwest::Error>{
-
-    let url = "https://www.ercot.com/content/cdr/html/20230308_dam_spp.html";
+async fn call_api(url: String) -> Result<String, reqwest::Error>{
 
     let res = reqwest::Client::new()
         .get(url)
